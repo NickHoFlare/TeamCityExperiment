0915591dcef6bffac20af3036b4afdf389a54586
terran324 <terran324@TeamCity>
2018-12-16 19:33:13 +1100

    TeamCity change in 'TeamCity Experiment' project: runners of 'HelloWorld' build configuration were updated

--- .teamcity/patches/buildTypes/HelloWorld.kts
+++ .teamcity/patches/buildTypes/HelloWorld.kts
@@ -2,7 +2,6 @@ package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
 import jetbrains.buildServer.configs.kotlin.v2018_2.BuildType
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.v2018_2.ui.*
 
 /*
@@ -17,15 +16,6 @@ create(DslContext.projectId, BuildType({
 
     vcs {
         root(DslContext.settingsRoot)
-    }
-
-    steps {
-        maven {
-            goals = "clean test"
-            pomLocation = ".teamcity/pom.xml"
-            runnerArgs = "-Dmaven.test.failure.ignore=true"
-            mavenVersion = defaultProvidedVersion()
-        }
     }
 }))
 
