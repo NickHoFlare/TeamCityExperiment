31294e221111b0626d18f3a882e71b8b8a825d99
Nicholas Ho <terran324@gmail.com>
2018-12-16 20:01:41 +1100

    Add 3 build steps and attach the build configuration to a build template

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,7 @@
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.powerShell
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.GitVcsRoot
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -26,13 +29,67 @@ version = "2018.2"
 
 project {
     description = "An experiment with using Kotlin DSL to create a build pipeline with TeamCity"
-    buildType {
-        id("HelloWorld")
-        name = "Hello world"
-        steps {
-            script {
-                scriptContent = "echo 'Hello world!'"
+
+    vcsRoot(TeamCityExperimentGitRepo)
+
+    buildType(HelloWorld)
+
+    template(TestTemplateConfiguration)
+}
+
+object HelloWorld : BuildType({
+    templates(TestTemplateConfiguration)
+    name = "HelloWorld"
+    description = "First Try"
+
+    vcs {
+        root(TeamCityExperimentGitRepo)
+    }
+
+    steps {
+        powerShell {
+            name = "Build"
+            id = "RUNNER_2"
+            scriptMode = script {
+                content = """echo "I am building...""""
             }
         }
+        powerShell {
+            name = "Test"
+            id = "RUNNER_3"
+            scriptMode = script {
+                content = """echo "I am testing...""""
+            }
+        }
+        powerShell {
+            name = "Deploy"
+            id = "RUNNER_4"
+            scriptMode = script {
+                content = """echo "I am deploying...""""
+            }
+        }
+        stepsOrder = arrayListOf("RUNNER_2", "RUNNER_3", "RUNNER_4", "RUNNER_5")
     }
-}
+})
+
+object TestTemplateConfiguration : Template({
+    name = "TestTemplateConfiguration"
+    description = "This is an example of a Template Configuration"
+
+    steps {
+        script {
+            name = "TestTemplateStep"
+            id = "RUNNER_5"
+            scriptContent = """echo "This is a test template build step""""
+        }
+    }
+})
+
+object TeamCityExperimentGitRepo : GitVcsRoot({
+    name = "TeamCityExperimentGitRepo"
+    url = "https://github.com/terran324/TeamCityExperiment.git"
+    authMethod = password {
+        userName = "terran324"
+        password = "credentialsJSON:c137106d-4250-4a8c-ba45-09b60635a868"
+    }
+})
