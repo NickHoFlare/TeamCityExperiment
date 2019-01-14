dc43366042703c9f50de8672504c72c56431eea7
Nicholas Ho <terran324@gmail.com>
2019-01-14 15:37:13 +1100

    Add param for repo name

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -30,6 +30,10 @@ version = "2018.2"
 project {
     description = "An experiment with using Kotlin DSL to create a build pipeline with TeamCity. TEST"
 
+    params {
+        param("repo.name", "TeamCityExperiment")
+    }
+
     vcsRoot(TeamCityExperimentGitRepo)
 
     buildType(HelloWorld)
