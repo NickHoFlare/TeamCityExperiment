174e258fe1403859be384728a0f5706db8544a80
Nicholas Ho <terran324@gmail.com>
2019-01-14 21:09:36 +1100

    fix another typo in subproject

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -91,7 +91,7 @@ object TestTemplateConfiguration : Template({
     }
 })
 
-object SubprojectTest : SubProject({
+object SubprojectTest : Project({
     id("SubprojectTest")
     name = "SubprojectTest"
     description = "An example of a subproject in TeamCity"
